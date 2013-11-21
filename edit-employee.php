f1981f183375b37111aa5eb44d1f7b1a055ba4a5
derek <ddekroon2@gmail.com>
2013-11-21 17:55:47 -0500

    adding thing

--- edit-employee.php
+++ edit-employee.php
@@ -1,12 +1,30 @@
 <?php require_once('connect.php');
 
-if(!($queryResult = $dbConnection->query("SELECT * FROM Employee"))) {
-	printf("Error - %s\n", $dbConnection->error);
+$commentObjs = array();
+
+if(!$dbFieldQuery = $dbConnection->query("SHOW COLUMNS FROM Employee")) {
+	echo 'Could not run query: ' . $dbConnection->error;
 }
 
-while($result = $queryResult->fetch_object()) {
-	$resultObj[] = $result;
-} ?>
+$fieldNames=array();
+if ($dbFieldQuery->num_rows > 0) {
+	while ($column = $dbFieldQuery->fetch_object()) {
+		$fieldNames[] =  $column->Field;
+	}
+} else {
+	print 'No available columns';
+	$pageFrame->printFooter();
+	exit(0);
+}
+$result = $dbConnection->query("SELECT * FROM Employee");
+if($result->num_rows > 0) {
+	while($obj = $result->fetch_object()) {
+		$commentObjs[] = $obj;
+	}
+}
+
+?>
+
 
 <html>
 	<head>
@@ -16,9 +34,26 @@ while($result = $queryResult->fetch_object()) {
 		<h1>CIS353 Databases Assignment 4</h1>
 		<h3>Dan Robinson, Trevor Baron, Derek Dekroon</h3>
 		<div class="container">
-			<?php foreach($resultObj as $employee) {
-				printf("~%s", $employee->Fname.' '.$employee->Lname);
-			} ?>
+			      <h1>Edit Employee Data</h1>
+      
+<?php 
+print '<table class="no-style" style="text-align:center;"><tr>';
+foreach($fieldNames as $field) {
+	print '<th>'.$field.'</th>';
+}
+print '</tr>';
+if(count($commentObjs) > 0) {
+	foreach($commentObjs as $commentSurvey) {
+		print '<tr>';
+		foreach($commentSurvey as $key => $value) {
+			print '<td>'.htmlentities($value, ENT_QUOTES).'</td>';
+		}
+		print '</tr>';
+	}
+} else {
+	print '<tr><td colspan='.$dbFieldQuery->num_rows.'>No data in the general comments table</td></tr>';
+}
+print '</table>';?>
 		</div>
 	</body>
 </html>
