ee6ac55c05b797ecf8fc84f6ab7458ae08e915ae
derek <ddekroon2@gmail.com>
2013-11-26 22:16:44 -0500

    minor update on header

--- view/create-dependent.php
+++ view/create-dependent.php
@@ -1,26 +1,14 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/view/include/header.php'); ?>
-
-<html>
-	<head>
-	<title>CIS3530A4</title>
-	</head>
-	<body>
-	<h1>CIS353 Databases Assignment 4</h1>
-	<h3>Dan Robinson, Trevor Baron, Derek Dekroon</h3>
-	<div class="container">
 	<h1>Create an Employee</h1>
 	<form method='post' id='createDepedentForm' action='create-dependent.php'>
-		<?php print '<table>';
-			echo implode(array_map(function($field){
-				if($field->Field != 'date_created') {
-					return '<tr><th>'.$field->Field.'</th><td><input name="'.$field->Field.'" type="text" /></td></tr>';
-				}
-			},$fields));
-		print '</table>'; ?>
-		<input type='submit' value='Create Employee' name='submitCreateDependent' />
-		</form>
-	</div>
-	</body>
-</html>
+	<?php print '<table>';
+		echo implode(array_map(function($field){
+			if($field->Field != 'date_created') {
+				return '<tr><th>'.$field->Field.'</th><td><input name="'.$field->Field.'" type="text" /></td></tr>';
+			}
+		},$fields));
+	print '</table>'; ?>
+	<input type='submit' value='Create Employee' name='submitCreateDependent' />
+	</form>
 
 <?php include($_SERVER['DOCUMENT_ROOT'].'/view/include/bottom.php'); ?>
--- view/edit-employee.php
+++ view/edit-employee.php
@@ -1,15 +1,6 @@
 <?php include($_SERVER['DOCUMENT_ROOT'].'/view/include/header.php'); ?>
-
-<html>
-	<head>
-	<title>CIS3530A4</title>
-	</head>
-	<body>
-	<h1>CIS353 Databases Assignment 4</h1>
-	<h3>Dan Robinson, Trevor Baron, Derek Dekroon</h3>
-	<div class="container">
+	
 	<h1>Edit Employee Data</h1>
-
 	<a href="create-employee.php" class="btn btn-hover" hidefocus="true" style="outline: none;">
 		<span>Create an Employee</span>
 	</a>
@@ -32,8 +23,5 @@
         	}
 		?>
 	</table>
-	</div>
-	</body>
-</html>
 
 <?php include($_SERVER['DOCUMENT_ROOT'].'/view/include/bottom.php'); ?>
